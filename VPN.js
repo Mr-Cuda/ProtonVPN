2b867b4c15fc930647478c937a4d05c9e6ec8ccd
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-06-30 11:07:49 +0200

    Update VPN.js

--- VPN.js
+++ VPN.js
@@ -1,16 +1,1 @@
-const axios = require('axios');
-const path = require('path');
-const crypto = require("crypto");
-const fs = require('fs');
-
-module.exports = (app) => {
-
-	app.get('/https://localhost:2374/vpn/vpnEngine/AuthorizationCode:7329', (req, res) => {
-		res.setHeader("content-type", "application/octet-stream")
-		res.sendFile(path.join(__dirname, '../balls/VpnEngine.ini'));
-	});
-
-	app.get('/presence/api/v1/_/:VPN/settings/bEnable=True', (req, res) => { res.status(204).end(); });
-};
-
-global.xmppClients = [''];
+shit
